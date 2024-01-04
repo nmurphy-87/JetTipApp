5bfc756616f17c0200c9a784445b5f674712e38d
Niall <niall@Aarons-MacBook-Air.local>
2024-01-04 17:09:28 +0000

    Calcuating tip amount successfully

---
+++ app/src/main/java/com/niallmurph/jettipapp/utils/Util.kt
@@ -0,0 +1,11 @@
+package com.niallmurph.jettipapp.utils
+
+
+fun calculateTotalTip(
+    totalBill: Double,
+    tipPercentage: Int
+): Double {
+    return if (totalBill > 1 && totalBill.toString()
+            .isNotEmpty()
+    ) (totalBill * tipPercentage) / 100 else 0.0
+}
