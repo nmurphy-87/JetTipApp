656b1ba462dcd83451129294354a3118457de83a
Niall <niall@Aarons-MacBook-Air.local>
2024-01-04 11:51:32 +0000

    Top Header Function - Finish Up

--- app/src/main/java/com/niallmurph/jettipapp/MainActivity.kt
+++ app/src/main/java/com/niallmurph/jettipapp/MainActivity.kt
@@ -3,20 +3,21 @@ package com.niallmurph.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.niallmurph.jettipapp.ui.theme.JetTipAppTheme
 
@@ -38,7 +39,7 @@ fun MyApp(content: @Composable () -> Unit) {
         Surface(
             color = MaterialTheme.colors.background
         ) {
-            content()
+            TopHeader()
         }
     }
 
@@ -46,15 +47,28 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson : Double = 0.0) {
     Surface(modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
-            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
         color = Color(0xFFE9D7f7)
     ) {
-        Column() {
-
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
